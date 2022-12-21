d67597a9daced5b4c35a02597588413ff86dd1b8
Markus Ahlstrand <markus@ahlstrand.de>
2022-12-21 13:35:36 +0100

    chore: remove node_modules

--- node_modules/@commitlint/prompt/lib/input.d.ts
+++
@@ -1,9 +0,0 @@
-import type { PromptModule } from 'inquirer';
-/**
- * Get user input by interactive prompt based on
- * conventional-changelog-lint rules.
- * @param prompter
- * @return commit message
- */
-export declare function input(prompter: PromptModule): Promise<string>;
-//# sourceMappingURL=input.d.ts.map
--- node_modules/@commitlint/prompt/lib/library/format.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Result } from './types';
-/**
- * Get formatted commit message
- * @param input object containing structured results
- * @param debug show debug information in commit message
- * @return formatted debug message
- */
-export default function format(input: Result, debug?: boolean): string;
-//# sourceMappingURL=format.d.ts.map
