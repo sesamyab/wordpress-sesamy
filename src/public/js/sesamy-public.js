b21e9a7972e368d8b8c8a2da624281ac20c77e41
Markus Ahlstrand <markus@ahlstrand.de>
2023-01-31 19:37:39 +0100

    fix: remove duplicate script import

--- src/public/js/sesamy-public.js
+++ src/public/js/sesamy-public.js
@@ -1,27 +1,25 @@
-(function( $ ) {
-	'use strict';
+(function ($) {
+  "use strict";
 
-	$('[data-sesamy-paywall]').each(function(){
+  $("[data-sesamy-paywall]").each(function () {
+    if (!sesamy) {
+      console.error("Sesamy JavaScript Api not available.");
+      return;
+    }
 
-		if(!sesamy){
-			console.error('Sesamy JavaScript Api not available.');
-			return;
-		}
+    var data = $(this).data();
 
-		var data = $(this).data();
+    if (!data.sesamyItemSrc) {
+      console.warn("Attribute data-sesamy-item-src not set.");
+      return;
+    }
 
-		if(!data.sesamyItemSrc){
-			console.warn('Attribute data-sesamy-item-src not set.');
-			return;
-		}		
-		
-		sesamy.getEntitlement(data.sesamyItemSrc, data.sesamyPasses?.split(',') ?? []).then(entitlement => {
-			if (entitlement !== undefined){
-				$(this).hide();
-			}
-		})	
-		
-
-	});
-
-})( jQuery );
+    sesamy
+      .getEntitlement(data.sesamyItemSrc, data.sesamyPasses?.split(",") ?? [])
+      .then((entitlement) => {
+        if (entitlement !== undefined) {
+          $(this).hide();
+        }
+      });
+  });
+})(jQuery);
