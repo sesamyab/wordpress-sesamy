b079d6187e330c41ed90ca1f2c0927e6e1565443
PradeepWeb77 <pradipweweb@gmail.com>
2023-12-07 19:35:20 +0530

    Added support for classic editor

--- src/admin/js/sesamy-admin.js
+++ src/admin/js/sesamy-admin.js
@@ -29,4 +29,59 @@
 	 * practising this, we should strive to set a better example in our own work.
 	 */
 
+	// Check the checkbox state on page load ( For Locked section )
+    if ($('.sesamy-classic-locked').is(':checked')) {
+    	var fromdate = $( "input[name=locked_from_date]" ).val();
+        $('.sesamy-classic-locked-active').hide(); // Show content if checkbox is unchecked
+        $('.sesamy-classic-locked-inactive').show(); // Hide content if checkbox is checked initially
+    }else{
+    	$('.sesamy-classic-locked-active').show(); // Show content if checkbox is unchecked
+    	if( fromdate == '' ){
+        	$('.sesamy-classic-locked-inactive').hide(); // Hide content if checkbox is checked initially
+        }else{
+        	$('.sesamy-classic-locked-inactive').show(); // Show content if from date not null
+        }
+    }
+
+    // Check checkbox change event
+    $( '.sesamy-classic-locked' ).change( function() {
+        var fromdate = $( "input[name=locked_from_date]" ).val();
+        if ( $( this ).is( ':checked' ) ) {
+            $('.sesamy-classic-locked-active').hide(); // Show content if checkbox is unchecked
+        	$('.sesamy-classic-locked-inactive').show(); // Hide content if checkbox is checked initially
+        } else {
+            $('.sesamy-classic-locked-active').show(); // Show content if checkbox is unchecked
+            if( fromdate == '' ){
+        		$('.sesamy-classic-locked-inactive').hide(); // Hide content if from date null
+        	}else{
+        		$('.sesamy-classic-locked-inactive').show(); // Show content if from date not null
+        	}
+        }
+    });
+
+    // On change from date
+    $( 'input[name=locked_from_date]' ).change( function() {
+    	if ( $( this ).val() == '' ){
+    		$('.sesamy-classic-locked-inactive').hide(); // Hide content if value of from date in null
+    	}else{
+    		$('.sesamy-classic-locked-inactive').show(); // Show content if checkbox is checked initially
+    	}
+    });
+
+    // Check the checkbox state on page load ( For Single Purchase Enable )
+    if ( $( '.sesamy-classic-single-purchase').is( ':checked' )) {
+        $('.sesamy-classic-locked-price').show(); // Show content if checkbox is checked initially
+    }else{
+    	$('.sesamy-classic-locked-price').hide(); // Hide content if checkbox is checked initially
+    }
+
+    // Check checkbox change event
+    $('.sesamy-classic-single-purchase').change( function() {
+        if ( $( this ).is(':checked') ) {
+            $('.sesamy-classic-locked-price').show(); // Hide content if checkbox is checked
+        } else {
+            $('.sesamy-classic-locked-price').hide(); // Show content if checkbox is unchecked
+        }
+    });
+
 })( jQuery );
