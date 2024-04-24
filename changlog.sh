69ff2e246d0eedb0e659c15f773db20f46507a60
PradeepWeb77 <pradipweweb@gmail.com>
2024-04-24 17:22:45 +0530

    Make change related to version in version and changelog file

--- changlog.sh
+++ changlog.sh
@@ -4,30 +4,7 @@ cd 'src'
 
 # Define the new version number (you can pass this as an argument or set it dynamically)
 # Get the last created tag from GitHub
-latest_tag=$(curl -s "https://api.github.com/repos/sesamyab/wordpress-sesamy/tags" | jq -r '.[7].name')
-
-# Parse the tag into major, minor, and patch components
-major=$(echo $latest_tag | cut -d. -f1)
-minor=$(echo $latest_tag | cut -d. -f2)
-patch=$(echo $latest_tag | cut -d. -f3)
-
-# Increment the patch version
-((patch++))
-
-# Get the last commit message
-commit_message=$(git log -1 --pretty=%b)
-
-# Check if the commit message contains keywords and adjust version accordingly
-if [[ $commit_message == *"major"* ]]; then
-    major=$((major + 1))
-elif [[ $commit_message == *"minor"* ]]; then
-    minor=$((minor + 1))
-elif [[ $commit_message == *"patch"* ]]; then
-    patch=$((patch + 1))
-fi
-
-# Construct the new version tag
-new_version="$major.$minor.$patch"
+new_version=$(curl -s "https://api.github.com/repos/sesamyab/wordpress-sesamy/tags" | jq -r '.[7].name')
 
 # Generate changelog
 # Replace the command below with the actual command to generate the changelog
