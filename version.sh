875457ea4dd18dc0bdb1c8f22276d898fb7ce242
PradeepWeb77 <pradipweweb@gmail.com>
2024-04-25 15:29:09 +0530

    Modify tag related changes and related files

--- version.sh
+++ version.sh
@@ -4,8 +4,9 @@ cd 'src'
 
 # Define the new version number (you can pass this as an argument or set it dynamically)
 # Get the last created tag from GitHub
-new_version=$(curl -s "https://api.github.com/repos/sesamyab/wordpress-sesamy/tags" | jq -r '.[7].name')
-
+#new_version=$(curl -s "https://api.github.com/repos/sesamyab/wordpress-sesamy/tags" | jq -r '.[7].name')
+git fetch --depth=1 origin +refs/tags/*:refs/tags/*
+new_version=$(git tag -l --sort=-v:refname | grep '^[0-9]\+\.[0-9]\+\.[0-9]\+$' | head -n 1)
 
 # Update version in sesamy.php
 sed -i "s/define( 'SESAMY_VERSION', '[0-9.]*' );/define( 'SESAMY_VERSION', '$new_version' );/" sesamy.php
