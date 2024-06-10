9614298747a0f2aa173c9ab657f4a4f497fa83e2
Julian Idiart <julianidiart1987@hotmail.com>
2024-06-10 15:54:38 +0200

    fix: sanitize & encode description (json compliant)

--- src/public/class-sesamy-content-container.php
+++ src/public/class-sesamy-content-container.php
@@ -227,7 +227,7 @@ class Sesamy_Content_Container {
 					"singlePurchase": {
 						"type": "single",
 						"text": "<?php echo $post->post_title; ?>",
-						"description": "<?php echo $single_purchase_description; ?>"
+						"description": <?php echo json_json_encode($single_purchase_description); ?>
 					},
 					<?php } ?>
 					<?php
@@ -239,7 +239,7 @@ class Sesamy_Content_Container {
 						{
 							"type": "<?php echo $pass['period']; ?>",
 							"text": "<?php echo $pass['title']; ?>",
-							"description": "<?php echo $pass['description']; ?>",
+							"description": <?php echo json_encode($pass['description']); ?>,
 							"price": "<?php echo $pass['price']; ?>",
 							"currency": "<?php echo $pass['currency']; ?>",
 							"recurringPaymentText": "<?php echo $pass['period']; ?>",
