03b46414ce8755f3c5031a6997cc5c83cbe06366
Michael Lindfors <micke.lindfors@gmail.com>
2024-06-14 12:14:51 +0200

    fix: added validation for bool values getting saved as bools

--- src/includes/class-sesamy-settings.php
+++ src/includes/class-sesamy-settings.php
@@ -29,7 +29,12 @@ class Sesamy_Settings {
 		register_setting( 'sesamy', 'sesamy_content_types' );
 		register_setting( 'sesamy', 'sesamy_tags' );
 		register_setting( 'sesamy', 'sesamy_client_id' );
-		register_setting( 'sesamy', 'sesamy_paywall_wizard' );
+		register_setting( 'sesamy', 'sesamy_paywall_wizard', 
+		array(
+			'type' => 'boolean',
+      'sanitize_callback' => 'rest_sanitize_boolean',
+      'default' => false, // Default value must be a boolean
+		) );
 		register_setting( 'sesamy', 'sesamy_paywall_wizard_logo_url' );
 		register_setting( 'sesamy', 'sesamy_paywall_wizard_title' );
 		register_setting( 'sesamy', 'sesamy_paywall_wizard_description' );
